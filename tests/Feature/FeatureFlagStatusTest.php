5ae456c18bd571afa0f49d31553b08bd655439a5
Tony Messias <tonyzrp@gmail.com>
2018-02-04 18:00:15 -0300

    Test disable flag validation rules

--- tests/Feature/FeatureFlagStatusTest.php
+++ tests/Feature/FeatureFlagStatusTest.php
@@ -31,7 +31,6 @@ class FeatureFlagStatusTest extends TestCase
 
     public function testCanDisableActiveFeatureFlags()
     {
-        $this->withoutExceptionHandling();
         Event::fake();
         $featureFlag = factory(FeatureFlag::class)->create([
             'value' => true,
@@ -52,5 +51,29 @@ class FeatureFlagStatusTest extends TestCase
         Event::assertDispatched(FlagWasDisabled::class, function (FlagWasDisabled $event) use ($featureFlag) {
             return $event->flag->is($featureFlag);
         });
+    }
+
+    public function testCannotDisableWithoutConfirmation()
+    {
+        $featureFlag = factory(FeatureFlag::class)->create([
+            'value' => true,
+        ]);
+
+        $response = $this->postJson(route('disabled-flags.store'), [
+            'feature_flag_id' => $featureFlag->getKey(),
+        ]);
+
+        $response->assertStatus(422);
+        $this->assertTrue($featureFlag->fresh()->value);
+    }
+
+    public function testFlagMustExist()
+    {
+        $response = $this->postJson(route('disabled-flags.store'), [
+            'feature_flag_id' => 123,
+            'confirmation' => '1',
+        ]);
+
+        $response->assertStatus(422);
     }
 }
